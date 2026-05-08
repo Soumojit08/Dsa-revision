939e77d74d61b037dc34f174e0e02a62ebf7f0d2
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-05-08 23:55:47 +0530

    bubble sort

--- session_1/src/BubbleSort.java
+++ session_1/src/BubbleSort.java
@@ -1,3 +1,25 @@
-public class BubbleSort {
+import java.util.Arrays;
 
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {8,10,1,5,6,2};
+
+        System.out.println("Unsorted Array : " + Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i]>arr[j]){
+                    swap(arr, i, j); //swap array elem if i elem large than j 10>1
+                }
+            }
+        }
+
+        System.out.println("Sorted Array : " + Arrays.toString(arr));
+    }
+
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
