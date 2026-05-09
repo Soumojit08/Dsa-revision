37ce6c647ec8ca5137caf1c2df5642776a58aa51
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-05-10 01:11:54 +0530

    Maximum Subarray sum

--- session_1/src/MaxSubArrSum.java
+++ session_1/src/MaxSubArrSum.java
@@ -1,3 +1,5 @@
+
+
 public class MaxSubArrSum {
     public static void main(String[] args) {
         int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
@@ -5,9 +7,25 @@ public class MaxSubArrSum {
         int maxSum = Integer.MIN_VALUE;
         int curSum = 0;
 
+        //to store the indx of return subarray
+        int start = 0;
+        int ansStart = 0;
+        int end = 0;
+
         for (int i = 0; i < arr.length; i++) {
-            curSum+=arr[i];
-            maxSum = Math.max(maxSum, curSum);
+
+            //current sum 0 so new subarray may start
+            if (curSum == 0){
+                start = i;
+            }
+
+            curSum += arr[i];
+
+            if (maxSum < curSum){
+                maxSum = curSum;
+                ansStart = start;
+                end = i;
+            }
 
             if (curSum < 0){
                 curSum = 0;
@@ -15,5 +33,9 @@ public class MaxSubArrSum {
         }
 
         System.out.println("Maximum Sum : " + maxSum);
+
+        for (int i = ansStart; i <= end; i++) {
+            System.out.print(arr[i] + " , ");
+        }
     }
 }
