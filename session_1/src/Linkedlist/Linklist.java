14823e381fd293ded5a3da6bbc9de04ef65eb377
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-05-12 20:28:44 +0530

    Linkedlist

---
+++ session_1/src/Linkedlist/Linklist.java
@@ -0,0 +1,63 @@
+package Linkedlist;
+
+public class Linklist {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    //when linklist initialised size zero as no elem added till now
+    public Linklist(){
+        this.size = 0;
+    }
+
+    //Node Structure
+    private class Node {
+        private int data; //private so that cant change without access
+        private Node next; //private so that cant change without access
+
+        public Node(int value) {
+            this.data = value;
+        }
+
+        public Node(int value, Node next){
+            this.data = value;
+            this.next = next;
+        }
+    }
+
+    public void insertFirst(int value){
+        Node node = new Node(value);
+        node.next = head;
+        head = node;
+
+        //when no elem in list then head and tail both null now tail point to head so that it can increase later
+        if (tail == null){
+            tail = head;
+        }
+
+        size++; //increase list size
+    }
+
+    public void insertEnd(int value){
+        Node node = new Node(value);
+
+        if (tail==null) insertFirst(value);
+
+        tail.next = node;
+        tail = node;
+
+        size++;
+    }
+
+    public void display(){
+        Node temp = head;
+
+        while (temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("End");
+
+    }
+}
---
+++ session_1/src/Linkedlist/Main.java
@@ -0,0 +1,11 @@
+package Linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        Linklist list = new Linklist();
+        list.insertFirst(10);
+        list.insertFirst(5);
+        list.insertFirst(1);
+        list.display();
+    }
+}
