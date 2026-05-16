b06e3098a948b8a3faa9a1c74219845a8c966326
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-05-16 09:35:25 +0530

    insert at pos

--- session_1/src/Linkedlist/Linklist.java
+++ session_1/src/Linkedlist/Linklist.java
@@ -7,7 +7,7 @@ public class Linklist {
     private int size;
 
     //when linklist initialised size zero as no elem added till now
-    public Linklist(){
+    public Linklist() {
         this.size = 0;
     }
 
@@ -20,29 +20,29 @@ public class Linklist {
             this.data = value;
         }
 
-        public Node(int value, Node next){
+        public Node(int value, Node next) {
             this.data = value;
             this.next = next;
         }
     }
 
-    public void insertFirst(int value){
+    public void insertFirst(int value) {
         Node node = new Node(value);
         node.next = head;
         head = node;
 
         //when no elem in list then head and tail both null now tail point to head so that it can increase later
-        if (tail == null){
+        if (tail == null) {
             tail = head;
         }
 
         size++; //increase list size
     }
 
-    public void insertEnd(int value){
+    public void insertEnd(int value) {
         Node node = new Node(value);
 
-        if (tail==null) insertFirst(value);
+        if (tail == null) insertFirst(value);
 
         tail.next = node;
         tail = node;
@@ -50,22 +50,32 @@ public class Linklist {
         size++;
     }
 
-    public void insertAtPos(int value, int position){
+    public void insertAtPos(int value, int position) {
         Node node = new Node(value);
-        Node temp = head;
 
-        for (int i=1; i < position-1; i++){
-            temp = temp.next;
+        if (position < 0 || position > size) System.out.println("Invalid pos");
+        else if (position == 1) {
+            insertFirst(value);
+        } else if (position == size) {
+            insertEnd(value);
+        } else {
+
+            Node temp = head;
+
+            for (int i = 1; i < position - 1; i++) {
+                temp = temp.next;
+            }
+            //till now temp is in prev node of the taget node
+
+            node.next = temp.next;
+            //now new node points to the taget pos
+            temp.next = node;
+            //temp node points to node now
+            size++;
         }
-        //till now temp is in prev node of the taget node
-
-        node.next = temp.next;
-        //now new node points to the taget pos
-        temp.next = node;
-        //temp node points to node now
     }
 
-    public void deleteFirst(){
+    public void deleteFirst() {
         if (head == null) System.out.println("No elem to delete");
         int elem = head.data;
 
@@ -74,21 +84,25 @@ public class Linklist {
         System.out.println("Deleted Element : " + elem);
     }
 
-    public void deleteEnd(){
+    public void deleteEnd() {
         if (head == null) System.out.println("No elem to delete");
 
         Node temp = head;
         //go till second last elem
-        while (temp.next.next != null){
+        while (temp.next.next != null) {
             temp = temp.next;
         }
         temp.next = null; //make the sec last node null so the last elem deleted
     }
 
-    public void display(){
+    public void deleteAtPos(int position) {
+
+    }
+
+    public void display() {
         Node temp = head;
 
-        while (temp != null){
+        while (temp != null) {
             System.out.print(temp.data + " -> ");
             temp = temp.next;
         }
