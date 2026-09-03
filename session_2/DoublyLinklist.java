286a86c704e614dae46f96b6b7e5af58af2ee336
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-09-03 11:24:22 +0530

    Doubly LinkList

--- session_2/DoublyLinklist.java
+++ session_2/DoublyLinklist.java
@@ -96,6 +96,95 @@ public class DoublyLinklist {
         System.out.println(" X ");
     }
 
+    public void printBackwardList() {
+        if (tail == null) {
+            System.out.println("Empty List");
+            return;
+        }
+        Node temp = tail;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.prev;
+        }
+        System.out.println(" X ");
+    }
+
+    public void deleteHead() {
+        if (head == null) {
+            System.out.println("Nothing to delete");
+            return;
+        }
+
+        // single node
+        if (head == tail) {
+            head = null;
+            tail = null;
+            size = 0;
+            return;
+        }
+
+        head = head.next;
+        head.prev = null;
+        size--;
+    }
+
+    public void deleteTail() {
+        if (head == null) {
+            System.out.println("Nothing to delete");
+            return;
+        }
+
+        // single node
+        if (head == tail) {
+            head = null;
+            tail = null;
+            size = 0;
+            return;
+        }
+
+        Node currNode = tail;
+        Node prevNode = currNode.prev;
+
+        // link change
+        prevNode.next = null;
+        currNode.prev = null;
+        tail = prevNode;
+        size--;
+    }
+
+    public void deleteAtPos(int pos) {
+        if (pos < 1 || pos > size + 1) {
+            System.out.println("Invalid Pos");
+            return;
+        }
+
+        if (pos == 1) {
+            deleteHead();
+            return;
+        }
+
+        if (pos == size) {
+            deleteTail();
+            return;
+        }
+
+        Node currNode = head;
+
+        for (int i = 1; i <= pos - 1; i++) {
+            currNode = currNode.next;
+        }
+
+        Node prevNode = currNode.prev;
+        Node nextNode = currNode.next;
+
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+        currNode.prev = null; 
+        currNode.next = null;
+        size--;
+
+    }
     public static void main(String[] args) {
         DoublyLinklist myList = new DoublyLinklist();
 
@@ -106,5 +195,14 @@ public class DoublyLinklist {
         myList.printForwardList();
         myList.insertAtPos(18, 4);
         myList.printForwardList();
+        myList.printBackwardList();
+        myList.deleteHead();
+        myList.deleteTail();
+        myList.deleteTail();
+        myList.printForwardList();
+        myList.insertAtHead(100);
+        myList.insertAtHead(200);
+        myList.insertAtHead(300);
+        myList.insertAtHead(100);
     }
 }
